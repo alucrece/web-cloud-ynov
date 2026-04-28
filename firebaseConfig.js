3a5c92eac1086413432eed2267aee9b4eec40352
alucrece <aliebonglucrece123@gmail.com>
2026-04-28 13:31:38 +0200

    chore: add firebase dependency to package.json

---
+++ firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { exp } from "firebase/firestore/pipelines";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAFvhta3Hrigqd4khrY1vNZhpgYF_ZylgY",
+  authDomain: "web-cloud-ynov-ccbdc.firebaseapp.com",
+  projectId: "web-cloud-ynov-ccbdc",
+  storageBucket: "web-cloud-ynov-ccbdc.firebasestorage.app",
+  messagingSenderId: "236983712572",
+  appId: "1:236983712572:web:63f7ecfabfb4d958807c39",
+  measurementId: "G-7K00YMG65H"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
