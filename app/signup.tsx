c527698fb08454a4482f25827d94f355d40de97e
alucrece <aliebonglucrece123@gmail.com>
2026-04-28 13:30:05 +0200

    Add Signup component for user registration with email and password

---
+++ app/signup.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import { signup } from "../firebase/auth_signup_password";
+
+export default function Signup() {
+  const [email, onChangeEmail] = React.useState("");
+  const [password, onChangePassword] = React.useState("");
+  return (
+    <View style={styles.container}>
+      <Text>Email</Text>
+        <TextInput
+            style={styles.input}
+            onChangeText={onChangeEmail}
+            value={email}
+        />
+      <Text>Password</Text>
+        <TextInput
+            style={styles.input}
+            onChangeText={onChangePassword}
+            value={password}
+            secureTextEntry={true}
+        />
+      <Button
+          title="Sign Up !"
+          onPress={() => signup(email, password)}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    input: {
+        height: 40,
+        width: 200,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
+});
